1394f905abe5664621c53637106035331c254453
Cristian Livella <cristian@cristianlivella.com>
2024-12-01 21:38:57 +0100

    Small fix

--- src/components/Scheduler/DailyColumnsSheet/EventContent.js
+++ src/components/Scheduler/DailyColumnsSheet/EventContent.js
@@ -13,7 +13,7 @@ function EventContent( { value, schedulerOptions } )
 
                 &nbsp;-&nbsp;
                 <span data-type="hour">
-                    { formatters['hh:ii'](value.end) }
+                    { formatters['hh:ii'](new Date(value.end.getTime() + diff * 60000)) }
                 </span>
 
             </div>
